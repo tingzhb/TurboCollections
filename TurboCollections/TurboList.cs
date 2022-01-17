a6abd0b85470986a3924168e420cdee02d30343d
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-17 15:05:13 +0100

    TurboTest

--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,6 +1,10 @@
-﻿namespace TurboCollections
-{
+﻿using System;
+
+namespace TurboCollections {
 	public class TurboList<T> {
-		
+		public int Count => 0;
+		public TurboList() {
+			Console.WriteLine("Hello Turbo");
+		}
 	}
 }
