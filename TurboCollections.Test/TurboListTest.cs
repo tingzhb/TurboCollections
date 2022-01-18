7887e9aff2cea3a09e09a3732e2696b7ea3fe39e
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-18 10:31:11 +0100

    Add Set, Clear, Remove At

--- TurboCollections.Test/TurboListTest.cs
+++ TurboCollections.Test/TurboListTest.cs
@@ -16,12 +16,39 @@ namespace TurboCollections.Test
 				list.Add(5);
 			Assert.AreEqual(numberOfElements, list.Count);
 		}
-
 		[Test]
 		public void AnAddedElementCanBeGotten() {
 			var list = new TurboList<int>();
 			list.Add(1337);
 			Assert.AreEqual(1337, list.Get(0));
-		} 
+		}
+		[Test]
+		public void AnItemCanbeReplaced() {
+			var list = new TurboList<int>();
+			list.Add(5);
+			list.Add(9);
+			list.Set(1, 2);
+			Assert.AreEqual(2, list.Get(1));
+		}
+		[Test]
+		public void ListCanBeCleared() {
+			var list = new TurboList<int>();
+			list.Add(5);
+			list.Add(9);
+			list.Clear();
+			Assert.AreEqual(0, list.Get(1));
+		}
+		[Test]
+		public void ItemCanBeRemoved() {
+			var list = new TurboList<int>();
+			list.Add(8);
+			list.Add(7);
+			list.Add(0);
+			list.Add(6);
+			list.Add(1);
+			list.RemoveAt(2);
+			Assert.AreEqual(6, list.Get(2));
+			// Assert.AreEqual(1, list.Get(4));
+		}
 	}
 }
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace TurboCollections {
 public class TurboList<T> {
 	public int Count => items.Length;
 	private T[] items = Array.Empty<T>();
+	
 	public void Add(T item) {
 		// Resize Array
 		T[] newArray = new T[Count + 1];
@@ -18,5 +20,33 @@ public class TurboList<T> {
 	public T Get(int index) {
 		return items[index];
 	}
+	public T Set(int index, T value) {
+		return items[index] = value;
+	}
+	public void Clear() {
+		// items = Array.Empty<T>();
+		for (int i = 0; i < Count; i++) {
+			items[i] = default;
+		}
+	}
+	public void RemoveAt(int index) {
+		for (int i = index; i < Count; i++) {
+			items[index] = items[index + 1];
+		}
+		T[] newArray = new T[Count - 1];
+		for (int i = 0; i < Count - 1; i++) {
+			newArray[i] = items[i];
+		}
+		items = newArray;
+	}
+	// public bool Contains(T item) {
+	// 	for (int i = 0; i < Count; i++) {
+	// 		if (items[i] == item) {
+	// 			return true;
+	// 		}
+	// 	}
+	// 	return false;
+	// }
+	
 }
 }
