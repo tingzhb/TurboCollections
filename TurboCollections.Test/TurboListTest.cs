c7e72df9114d63cc230fb06bbe9760f9c20fd2a0
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-18 14:59:16 +0100

    TurboList Testing

--- TurboCollections.Test/TurboListTest.cs
+++ TurboCollections.Test/TurboListTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Dynamic;
 using NUnit.Framework;
 
 namespace TurboCollections.Test {
@@ -21,6 +23,21 @@ namespace TurboCollections.Test {
 			Assert.AreEqual(1337, list.Get(0));
 		}
 		[Test]
+		public void MultipleAddedElementsCanBeGotten() {
+			var (numbers, list) = CreateTestData();
+			for (int i = 0; i < numbers.Length; i++) {
+				Assert.AreEqual(numbers[i], list.Get(i));
+			}
+		}
+		(int[] numbers, TurboList<int>) CreateTestData() {
+			int[] numbers = {8, 7, 0, 6, 1, 0, 8, 3, 3, 2};
+			var list = new TurboList<int>();
+			foreach (var number in numbers) {
+				list.Add(number);
+			}
+			return (numbers, list);
+		}
+		[Test]
 		public void AnItemCanbeReplaced() {
 			var list = new TurboList<int>();
 			list.Add(5);
@@ -29,12 +46,49 @@ namespace TurboCollections.Test {
 			Assert.AreEqual(2, list.Get(1));
 		}
 		[Test]
-		public void ListCanBeCleared() {
-			var list = new TurboList<int>();
-			list.Add(5);
-			list.Add(9);
+		public void CanNotBeExtendedBySetting() {
+			const int setIndex = 100;
+			var (_, list) = CreateTestData();
+			Assert.Throws<IndexOutOfRangeException>(() => list.Set(setIndex, 666));
+		}
+		[Test]
+		public void IsEmptyAfterClearing() {
+			var (_, list) = CreateTestData();
 			list.Clear();
-			// Assert.AreEqual(0, list.Get(1));
+			Assert.Zero(list.Count);
+		}
+		[Test]
+		public void ItemIsAddedAtIndexZeroAfterClearing() {
+			var (_, list) = CreateTestData();
+			list.Clear();
+			list.Add(5);
+			Assert.AreEqual(1, list.Count);
+			Assert.AreEqual(5, list.Get(0));
+		}
+		[Test]
+		public void ItemsAreClearedWhenClearing() {
+			// Given
+			var (numbers, list) = CreateTestData();
+			// When
+			list.Clear();
+			// Then
+			for (int i = 0; i < numbers.Length; i++) {
+				Assert.Zero(list.Get(i));
+			}
+		}
+		[Test]
+		public void CountIsReducedWhenRemovingAt() {
+			var (numbers, list) = CreateTestData();
+			list.RemoveAt(2);
+			Assert.AreEqual(numbers.Length - 1, list.Count);
+		}
+		[Test]
+		public void ItemsAreMovedForwardWhenRemovingAt() {
+			var (numbers, list) = CreateTestData();
+			list.RemoveAt(2);
+			for (int i = 2; i < numbers.Length - 1; i++) {
+				Assert.AreEqual(numbers[i + 1], list.Get(i), $"Wrong item at index {i}");
+			}
 		}
 		[Test]
 		public void ItemCanBeRemoved() {
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,56 +1,114 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace TurboCollections {
 public class TurboList<T> {
-	public int Count => items.Length;
+	public int Count { get; private set; }
 	private T[] items = Array.Empty<T>();
 	
 	public void Add(T item) {
 		// Resize Array
-		T[] newArray = new T[Count + 1];
+		EnsureSize(Count + 1);
+		items[Count++] = item;
+	}
+	
+	/// <summary>
+	/// This method ensures that the array is at least 'size' big.
+	/// </summary>
+	/// <param name="size">The size that your array should have</param>
+	void EnsureSize(int size) {
+		// If the array is large enough return
+		if (items.Length > size)
+			return;
+		// Double the array size, or set it to given size if doubling is not enough.
+		int newSize = Math.Max(size, items.Length * 2);
+		
+		T[] newArray = new T[newSize];
 		for (int i = 0; i < Count; i++) {
 			newArray[i] = items[i];
 		}
-		// Assign new element
-		newArray[Count] = item;
 		items = newArray;
 	}
 	public T Get(int index) {
-		if (index > Count) {
-			throw new Exception("Cannot GET a non-existent item!");
-		}
 		return items[index];
 	}
-	public void Set(int index, T value) {
-		if (index > Count) {
-			throw new Exception("Cannot SET a non-existent item!");
-		}
-		items[index] = value;
+	public void Set(int index, T item) {
+		items[index] = item;
 	}
 	public void Clear() {
-		items = Array.Empty<T>();
+		for (int i = 0; i < Count; i++) {
+			items[i] = default;
+		}
+		Count = 0;
 	}
 	public void RemoveAt(int index) {
-		for (int i = index; i < Count; i++) {
-			items[index] = items[index + 1];
+		for (int i = index; i < Count - 1; i++) {
+			items[i] = items[i + 1];
 		}
-		T[] newArray = new T[Count - 1];
-		for (int i = 0; i < Count - 1; i++) {
-			newArray[i] = items[i];
-		}
-		items = newArray;
+		Count--;
 	}
-	// public bool Contains(T item) {
-	// 	for (int i = 0; i < Count; i++) {
-	// 		if (items[i] == item) {
-	// 			return true;
-	// 		}
-	// 	}
-	// 	return false;
-	// }
+	public bool Contains(T item) {
+		return IndexOf(item) != -1;
+	}
+	
+	public int IndexOf(T item) {
+		for (int i = 0; i < Count; i++) {
+			if (Equals(item, items[i])) {
+				return i;
+			}
+		}
+		return - 1;
+	}
+
+	public void Remove(T item) {
+		var index = IndexOf(item);
+		if (index == -1)
+			return; 
+		RemoveAt(index);
+	}
 	public void AddRange(IEnumerable<T> items) {
-		
+		foreach (var item in items) {
+			Add(item);
+		}
 	}
+
+	// public Enumerator GetEnumerator() {
+	// 	return new Enumerator(items, Count);
+	// }
+	//
+	// IEnumerator<T> IEnumerable<T>.GetEnumerator();
+	//
+	// public struct Enumerator<T> : IEnumerator<T> {
+	//
+	// 	private readonly T[] _items;
+	// 	private readonly int _count;
+	// 	private int _index;
+	// 	public Enumerator(T[] items, int count) {
+	// 		_items = items;
+	// 		_count = count;
+	// 		_index = 1;
+	// 		Current = default;
+	// 	}
+	// 	public bool MoveNext() {
+	// 		if (_index >= _count) {
+	// 			return false;
+	// 		}
+	// 		return ++_index < _count;
+	// 	}
+	// 	public void Reset() {
+	// 		
+	// 	}
+	// 	public T Current { get; }
+	// 	object IEnumerator.Current => Current;
+	// 	public void Dispose() {
+	// 		throw new NotImplementedException();
+	// 	}
+	// }
+	//
+	// public IEnumerator<T> GetEnumerator() {
+	// 	
+	// }
 }
 }
